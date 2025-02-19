0f9c1c719c3213f2d9c3d481d7ee148e30a238c9
Netamine <netamine@hotmail.com>
2025-02-19 09:48:30 -0500

    Correction de run_flask.py pour Render

--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 from waitress import serve
+import time
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ def home():
     return "✅ Test Flask API is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Récupérer le port depuis Render
-    print(f"Lancement du serveur Flask avec Waitress sur le port {port}...")
+    port = int(os.environ.get("PORT", 5000))  # Récupération du port de Render
+    print(f"🔄 Attente avant le lancement du serveur...")
+    time.sleep(5)  # Attendre 5 secondes pour laisser Render scanner les ports
+    print(f"🚀 Lancement du serveur Flask avec Waitress sur le port {port}...")
     serve(app, host="0.0.0.0", port=port)
